e4e0c6f2f76ffea57750ac166b3c8f3d7c50c1ad
toddrgarner <110719370+toddrgarner@users.noreply.github.com>
2022-09-13 01:17:37 -0700

    added 3 array questions in index.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -34,6 +34,7 @@ module.exports = generateMarkdown;
 // If your README is long, add a table of contents to make it easy for users to find what they need.
 
 // - [Installation](#installation)
+
 // - [Usage](#usage)
 // - [Credits](#credits)
 // - [License](#license)
