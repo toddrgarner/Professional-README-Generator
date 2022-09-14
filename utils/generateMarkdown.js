a58343976c288241591a8bd71da554af592f52e2
toddrgarner <110719370+toddrgarner@users.noreply.github.com>
2022-09-14 15:52:25 -0700

    added MIT, Apache & BSD-3

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,33 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge() {}
+function renderLicenseBadge(data) {
+  var licensePicked = data.License;
+  let licenseBadge = ''
+  if (licensePicked === "MIT"){
+    licenseBadge = "[![License](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+    return licenseBadge; 
+  }
+
+  if (licensePicked === "Apache"){
+    licenseBadge = "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+    return licenseBadge; 
+  }
+
+  if (licensePicked === "BSD-3 Clause"){
+    licenseBadge = "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
+    return licenseBadge; 
+  }
+
+  if (licensePicked === "None"){
+    return; 
+  }
+
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink() {}
+// function renderLicenseLink() {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -12,7 +35,7 @@ function renderLicenseSection() {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.Title}
 `;
 }
 
